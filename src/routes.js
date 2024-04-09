50874c127a0c26d3b7aad4e7a6dbfaa7914fad0c
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-04-09 11:54:07 +0200

    create not found page

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,21 @@ import StandardPage from 'components/StandardPage';
 import About from 'pages/About';
 import Menu from 'pages/Cardapio';
 import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
 	return (
-		<main>
+		<main className='container'>
 			<Router>
 				<Navbar />
 				<Routes>
 					<Route path='/' element={<StandardPage />}>
 						<Route index element={<Home />} />
 						<Route path='menu' element={<Menu />}/>
-						<Route path='about' element={<About />}/>
-					</Route>					
+						<Route path='about' element={<About />}/>						
+					</Route>
+					<Route path='*' element={<NotFound />} />					
 				</Routes>
 				<Footer />
 			</Router>
